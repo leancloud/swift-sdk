36aa9bfb4939bc99fdf0b7819a9a23fe716b3723
Tianyong Tang <tang3w@gmail.com>
2016-02-22 18:01:05 +0800

    Add swift project

---
+++ LeanCloud/LeanCloud/LeanCloud.h
@@ -0,0 +1,19 @@
+//
+//  LeanCloud.h
+//  LeanCloud
+//
+//  Created by Tang Tianyong on 2/22/16.
+//  Copyright © 2016 LeanCloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LeanCloud.
+FOUNDATION_EXPORT double LeanCloudVersionNumber;
+
+//! Project version string for LeanCloud.
+FOUNDATION_EXPORT const unsigned char LeanCloudVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeanCloud/PublicHeader.h>
+
+
