bc80a3ca4e0154ed92814f03a8144cc8ad8e0b62
Tianyong Tang <tang3w@gmail.com>
2016-02-23 13:23:22 +0800

    Add configuration

---
+++ Source/Configuration.swift
@@ -0,0 +1,16 @@
+//
+//  Configuration.swift
+//  LeanCloud
+//
+//  Created by Tang Tianyong on 2/23/16.
+//  Copyright © 2016 LeanCloud. All rights reserved.
+//
+
+import Foundation
+
+class Configuration {
+    static let sharedInstance = Configuration()
+
+    var applicationID:  String?!
+    var applicationKey: String?!
+}
