7679af131a8380b9c44b1b59246fdfb135fefd89
Tianyong Tang <tang3w@gmail.com>
2016-02-24 17:49:03 +0800

    Add object operation and operation control

---
+++ Source/ObjectOperation.swift
@@ -0,0 +1,13 @@
+//
+//  ObjectOperation.swift
+//  LeanCloud
+//
+//  Created by Tang Tianyong on 2/24/16.
+//  Copyright © 2016 LeanCloud. All rights reserved.
+//
+
+import Foundation
+
+class ObjectOperation {
+    /* Stub class */
+}
---
+++ Source/ObjectOperationControl.swift
@@ -0,0 +1,13 @@
+//
+//  ObjectOperationControl.swift
+//  LeanCloud
+//
+//  Created by Tang Tianyong on 2/24/16.
+//  Copyright © 2016 LeanCloud. All rights reserved.
+//
+
+import Foundation
+
+class ObjectOperationControl {
+    /* Stub class */
+}
