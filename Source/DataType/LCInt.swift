daa85cb7b785cb5027ff2c71aba9e15c75ada6e8
Tianyong Tang <tang3w@gmail.com>
2016-03-01 09:42:04 +0800

    Add increment method for LCInt

--- Source/DataType/LCInt.swift
+++ Source/DataType/LCInt.swift
@@ -14,13 +14,27 @@ import Foundation
  It is a wrapper of Int type, used to store an integer value.
  */
 public class LCInt: LCType {
+    public var value = 0
 
     /**
      Increase integer value by 1.
      */
     public func increase() {
+        value += 1
         updateParent { (object, key) -> Void in
-            object.addOperation(.Increment, key, nil)
+            object.addOperation(.Increment, key, 1)
+        }
+    }
+
+    /**
+     Increase integer value by specified amount.
+
+     - parameter amount: The amount to increase.
+     */
+    public func increaseBy(amount: Int) {
+        value += amount
+        updateParent { (object, key) -> Void in
+            object.addOperation(.Increment, key, amount)
         }
     }
 }
