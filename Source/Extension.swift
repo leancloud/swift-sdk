63905aa8c02f0ed47aa517c5802c42d2d62a6cee
Tianyong Tang <tang3w@gmail.com>
2016-03-24 16:17:41 +0800

    Add some operator for array

---
+++ Source/Extension.swift
@@ -0,0 +1,49 @@
+//
+//  Extension.swift
+//  LeanCloud
+//
+//  Created by Tang Tianyong on 3/24/16.
+//  Copyright © 2016 LeanCloud. All rights reserved.
+//
+
+import Foundation
+
+func +<T: LCType>(left: [T]?, right: [T]?) -> [T]? {
+    if let right = right {
+        var result = left ?? []
+
+        result.appendContentsOf(right)
+
+        return result
+    } else {
+        return left
+    }
+}
+
+func +~<T: LCType>(left: [T]?, right: [T]?) -> [T]? {
+    if let right = right {
+        var result = left ?? []
+
+        right.forEach { (element) in
+            if !result.contains(element) {
+                result.append(element)
+            }
+        }
+
+        return result
+    } else {
+        return left
+    }
+}
+
+func -<T: LCType>(left: [T]?, right: [T]?) -> [T]? {
+    if let left = left {
+        if let right = right {
+            return left.filter { !right.contains($0) }
+        } else {
+            return left
+        }
+    } else {
+        return nil
+    }
+}
