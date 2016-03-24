81d2a894f60c7ba31d8b70fa699af58fc4c1e693
Tianyong Tang <tang3w@gmail.com>
2016-03-24 16:28:25 +0800

    Add relation type

---
+++ Source/DataType/LCRelation.swift
@@ -0,0 +1,41 @@
+//
+//  LCRelation.swift
+//  LeanCloud
+//
+//  Created by Tang Tianyong on 3/24/16.
+//  Copyright © 2016 LeanCloud. All rights reserved.
+//
+
+import Foundation
+
+public final class LCRelation: LCType {
+    typealias Element = LCObject
+
+    var value: [Element]?
+
+    public override func copyWithZone(zone: NSZone) -> AnyObject {
+        return self
+    }
+
+    override func forEachChild(body: (child: LCType) -> Void) {
+        value?.forEach { body(child: $0) }
+    }
+
+    /**
+     Append an element.
+
+     - parameter element: The element to be appended.
+     */
+    func append(element: Element) {
+        self.value = self.value + [element]
+    }
+
+    /**
+     Remove an element from list.
+
+     - parameter element: The element to be removed.
+     */
+    func remove(element: Element) {
+        self.value = self.value - [element]
+    }
+}
