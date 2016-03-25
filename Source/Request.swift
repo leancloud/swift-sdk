1d7f4b12fdeb4bee2405345a19a0d8e03d065edf
Tianyong Tang <tang3w@gmail.com>
2016-03-25 17:13:25 +0800

    Implement request JSON representation

--- Source/Request.swift
+++ Source/Request.swift
@@ -12,9 +12,79 @@ class Request {
     let object: LCObject
     let operationTable: OperationTable
 
+    /// REST API version
+    static let APIVersion = "1.1"
+
     init(object: LCObject, operationTable: OperationTable) {
         self.object = object
         self.operationTable = operationTable
+    }
+
+    var isNew: Bool {
+        return object.objectId == nil
+    }
+
+    var method: String {
+        return isNew ? "POST" : "PUT"
+    }
+
+    var path: String {
+        return "/\(Request.APIVersion)/classes/\(object.dynamicType.className())"
+    }
+
+    var body: AnyObject {
+        var body: [String: AnyObject] = [
+            "__internalId": object.objectId ?? object.internalId
+        ]
+
+        var children: [(String, LCObject)] = []
+
+        operationTable.forEach { (key, operation) in
+            switch operation.name {
+            case .Set:
+                /* If object is newborn, put it in __children field. */
+                if let child = operation.value as? LCObject {
+                    if child.objectId == nil {
+                        children.append((key, child))
+                        break
+                    }
+                }
+
+                body[key] = operation.JSONValue()
+            default:
+                body[key] = operation.JSONValue()
+            }
+        }
+
+        if children.count > 0 {
+            var list: [AnyObject] = []
+
+            children.forEach { (key, child) in
+                list.append([
+                    "className": child.dynamicType.className(),
+                    "cid": child.internalId,
+                    "key": key
+                ])
+            }
+
+            body["__children"] = list
+        }
+
+        return body
+    }
+
+    func JSONValue() -> AnyObject {
+        var request: [String: AnyObject] = [
+            "path": path,
+            "method": method,
+            "body": body
+        ]
+
+        if isNew {
+            request["new"] = true
+        }
+
+        return request
     }
 }
 
