b45255aab425041cc6d7dbd31fe0a623fe0590c3
Tianyong Tang <tang3w@gmail.com>
2016-03-28 16:40:53 +0800

    Add type for HTTP response

---
+++ Source/Response.swift
@@ -0,0 +1,18 @@
+//
+//  Response.swift
+//  LeanCloud
+//
+//  Created by Tang Tianyong on 3/28/16.
+//  Copyright © 2016 LeanCloud. All rights reserved.
+//
+
+import Foundation
+
+public class Error {
+    /* Stub class. */
+}
+
+public enum BooleanResultType {
+    case Success
+    case Failure(error: Error)
+}
