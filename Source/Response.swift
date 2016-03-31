a83bb88a4f2f1516ebf8905e7095116af4dffd1c
Tianyong Tang <tang3w@gmail.com>
2016-03-31 13:34:09 +0800

    Add isSuccess property to response

--- Source/Response.swift
+++ Source/Response.swift
@@ -10,9 +10,25 @@ import Foundation
 import Alamofire
 
 public class Response {
-    let alamofireResponse: Alamofire.Response<AnyObject, NSError>
+    let alamofireResponse: Alamofire.Response<AnyObject, NSError>?
+
+    init() {
+        alamofireResponse = nil
+    }
 
     init(_ alamofireResponse: Alamofire.Response<AnyObject, NSError>) {
         self.alamofireResponse = alamofireResponse
+    }
+
+    /**
+     A boolean property indicates whether response is OK or not.
+     */
+    public var isSuccess: Bool {
+        if let response = alamofireResponse {
+            /* TODO: Handle the business error. */
+            return response.result.isSuccess
+        } else {
+            return true
+        }
     }
 }
