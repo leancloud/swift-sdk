f092ed2687437d5f3d01677e433616d83963690c
Tianyong Tang <tang3w@gmail.com>
2016-03-31 08:24:28 +0800

    Add common headers for REST request

--- Source/RESTClient.swift
+++ Source/RESTClient.swift
@@ -33,14 +33,59 @@ class RESTClient {
         }
     }
 
+    /// Header field name.
+    class HeaderFieldName {
+        static let ID         = "X-LC-Id"
+        static let Signature  = "X-LC-Sign"
+        static let Session    = "X-LC-Session"
+        static let Production = "X-LC-Prod"
+        static let UserAgent  = "User-Agent"
+        static let Accept     = "Accept"
+    }
+
     /// REST API version.
     static let APIVersion = "1.1"
+
+    /// Default timeout interval of each request.
+    static let defaultTimeoutInterval: NSTimeInterval = 10
 
     /// REST client shared instance.
     static let sharedInstance = RESTClient()
 
     /// Request dispatch queue.
-    static let queue = dispatch_queue_create("LeanCloud.REST", DISPATCH_QUEUE_CONCURRENT)
+    static let dispatchQueue = dispatch_queue_create("LeanCloud.REST", DISPATCH_QUEUE_CONCURRENT)
+
+    /// Shared request manager.
+    static var requestManager: Alamofire.Manager = {
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        configuration.timeoutIntervalForRequest = defaultTimeoutInterval
+        return Manager(configuration: configuration)
+    }()
+
+    /// User agent of SDK.
+    static let userAgent = "LeanCloud Swift-\(Version) SDK"
+
+    /// Signature of each request.
+    static var signature: String {
+        let timestamp = String(format: "%.0f", 1000 * NSDate().timeIntervalSince1970)
+        let hash = "\(timestamp)\(Configuration.sharedInstance.applicationKey)".MD5String.lowercaseString
+
+        return "\(hash),\(timestamp)"
+    }
+
+    /// Common REST request headers.
+    static var commonHeaders: [String:String] {
+        let headers: [String:String] = [
+            HeaderFieldName.ID:        Configuration.sharedInstance.applicationID,
+            HeaderFieldName.Signature: self.signature,
+            HeaderFieldName.UserAgent: self.userAgent,
+            HeaderFieldName.Accept:    "application/json"
+        ]
+
+        /* TODO: Add user session and production mode fields etc. */
+
+        return headers
+    }
 
     /// REST host for current service region.
     static var host: String {
@@ -56,8 +101,8 @@ class RESTClient {
      - parameter endpoint: The REST API endpoint.
      - returns: An absolute REST API URL string.
      */
-    func URLString(endpoint: String) -> String {
-        return "https://\(RESTClient.host)/\(RESTClient.APIVersion)/\(endpoint)"
+    static func URLString(endpoint: String) -> String {
+        return "https://\(self.host)/\(self.APIVersion)/\(endpoint)"
     }
 
     /**
@@ -66,7 +111,7 @@ class RESTClient {
      - parameter method:            The HTTP Method.
      - parameter endpoint:          The REST API endpoint.
      - parameter parameters:        The request parameters.
-     - parameter completionHandler: The completion handler.
+     - parameter completionHandler: The completion callback closure.
 
      - returns: A request object.
      */
@@ -74,14 +119,15 @@ class RESTClient {
         method: Method,
         endpoint: String,
         parameters: [String: AnyObject]? = nil,
-        handler: (Response) -> Void)
+        completionHandler: (Response) -> Void)
         -> Request
     {
-        let URLString = self.URLString(endpoint)
-        let request = Alamofire.request(method.alamofireMethod, URLString, parameters: parameters)
+        let manager   = RESTClient.requestManager
+        let URLString = RESTClient.URLString(endpoint)
+        let request   = manager.request(method.alamofireMethod, URLString, parameters: parameters, encoding: .JSON, headers: RESTClient.commonHeaders)
 
-        request.responseJSON(queue: RESTClient.queue) { response in
-            handler(Response(response))
+        request.responseJSON(queue: RESTClient.dispatchQueue) { response in
+            completionHandler(Response(response))
         }
 
         return Request(request)
