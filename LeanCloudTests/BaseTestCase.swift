131cd489517a417c1e017f14d2a2b871a0d39c1f
Tianyong Tang <tang3w@gmail.com>
2016-04-18 09:54:34 +0800

    Add base test case

--- LeanCloudTests/BaseTestCase.swift
+++ LeanCloudTests/BaseTestCase.swift
@@ -1,6 +1,6 @@
 //
-//  LeanCloudTests.swift
-//  LeanCloudTests
+//  BaseTestCase.swift
+//  BaseTestCase
 //
 //  Created by Tang Tianyong on 2/22/16.
 //  Copyright © 2016 LeanCloud. All rights reserved.
@@ -9,7 +9,7 @@
 import XCTest
 @testable import LeanCloud
 
-class LeanCloudTests: XCTestCase {
+class BaseTestCase: XCTestCase {
     
     override func setUp() {
         super.setUp()
@@ -19,18 +19,6 @@ class LeanCloudTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }
