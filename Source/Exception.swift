c2d66747f669413e03b0df4c6eee4a8995986b54
Tianyong Tang <tang3w@gmail.com>
2016-05-03 15:14:03 +0800

    Add exception class

---
+++ Source/Exception.swift
@@ -0,0 +1,23 @@
+//
+//  Exception.swift
+//  LeanCloud
+//
+//  Created by Tang Tianyong on 5/3/16.
+//  Copyright © 2016 LeanCloud. All rights reserved.
+//
+
+import Foundation
+
+class Exception {
+    enum Name: String {
+        case InvalidType
+    }
+
+    static func raise(name: String, reason: String? = nil, userInfo: [NSObject: AnyObject]? = nil) {
+        NSException(name: name, reason: reason, userInfo: userInfo).raise()
+    }
+
+    static func raise(name: Name, reason: String? = nil, userInfo: [NSObject: AnyObject]? = nil) {
+        raise(name.rawValue, reason: reason, userInfo: userInfo)
+    }
+}
