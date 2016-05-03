2df97e6edee6bad0a1456d608f0662f0a1adf9db
Tianyong Tang <tang3w@gmail.com>
2016-05-03 17:04:33 +0800

    Remove LCParent

--- Source/DataType/LCType.swift
+++ Source/DataType/LCType.swift
@@ -12,11 +12,6 @@ infix operator +~ {
     associativity left
 }
 
-struct LCParent {
-    weak var object: LCType?
-    let      propertyName: String
-}
-
 func + (left: LCType, right: LCType?) -> LCType? {
     return left.add(right)
 }
@@ -34,33 +29,11 @@ func & (left: LCType, right: LCType?) -> LCType? {
 }
 
 /**
- Check whether two parents are unequal.
-
- - parameter left:  Left parent.
- - parameter right: Right parent.
-
- - returns: true if two parents are unequal, false otherwise.
- */
-func != (
-    left:  LCParent,
-    right: LCParent
-) -> Bool {
-    return (left.object != right.object) || (left.propertyName != right.propertyName)
-}
-
-/**
  LeanCloud abstract data type.
  
  It is superclass of all LeanCloud data type.
  */
 public class LCType: NSObject, NSCopying {
-    /// Parent object.
-    var parent: LCParent? {
-        willSet {
-            validateParent(newValue)
-        }
-    }
-
     var JSONValue: AnyObject? {
         Exception.raise(.InvalidType, reason: "No JSON representation.")
         return nil
@@ -78,22 +51,6 @@ public class LCType: NSObject, NSCopying {
 
     public func copyWithZone(zone: NSZone) -> AnyObject {
         return self
-    }
-
-    /**
-     Validate parent.
-
-     A LCType object can be bound to another LCType object's property.
-     It can only be bound once. We add this constraint for consistency.
-
-     - parameter parent: The parent to validate.
-     */
-    func validateParent(parent: LCParent?) {
-        if let previousParent = self.parent {
-            if parent == nil || parent! != previousParent {
-                /* TODO: throw an exception that parent cannot be altered once bound. */
-            }
-        }
     }
 
     /**
