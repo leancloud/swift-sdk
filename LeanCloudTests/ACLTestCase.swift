058abf3a4d373fff32e10559fe9eb111025e85f8
Tianyong Tang <tang3w@gmail.com>
2016-05-07 09:06:57 +0800

    Add ACL type

---
+++ LeanCloudTests/ACLTestCase.swift
@@ -0,0 +1,55 @@
+//
+//  ACLTestCase.swift
+//  LeanCloud
+//
+//  Created by Tang Tianyong on 5/6/16.
+//  Copyright © 2016 LeanCloud. All rights reserved.
+//
+
+import XCTest
+@testable import LeanCloud
+
+class ACLTestCase: BaseTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testGetAndSet() {
+        let acl = LCACL()
+
+        /* Update access permission for public. */
+        XCTAssertFalse(acl.getAccess(.Read))
+        acl.setAccess(.Read, allowed: true)
+        XCTAssertTrue(acl.getAccess(.Read))
+        XCTAssertFalse(acl.getAccess([.Read, .Write]))
+        acl.setAccess(.Write, allowed: true)
+        XCTAssertTrue(acl.getAccess(.Write))
+        XCTAssertTrue(acl.getAccess([.Read, .Write]))
+
+        let userID   = "1"
+        let roleName = "2"
+        let readKey  = "read"
+        let writeKey = "write"
+        let roleAccessKey = LCACL.accessKey(roleName: roleName)
+
+        /* Update access permission for user. */
+        acl.setAccess([.Read, .Write], allowed: true, forUserID: userID)
+        XCTAssertTrue(acl.getAccess([.Read, .Write], forUserID: userID))
+        acl.setAccess(.Write, allowed: false, forUserID: userID)
+        XCTAssertFalse(acl.getAccess(.Write, forUserID: userID))
+
+        /* Update access permission for role. */
+        acl.setAccess([.Read, .Write], allowed: true, forRoleName: roleName)
+        XCTAssertEqual(acl.value[roleAccessKey]!, [readKey: true, writeKey: true])
+        acl.setAccess(.Write, allowed: false, forRoleName: roleName)
+        XCTAssertEqual(acl.value[roleAccessKey]!, [readKey: true])
+    }
+
+}
---
+++ Source/DataType/LCACL.swift
@@ -0,0 +1,183 @@
+//
+//  LCACL.swift
+//  LeanCloud
+//
+//  Created by Tang Tianyong on 5/4/16.
+//  Copyright © 2016 LeanCloud. All rights reserved.
+//
+
+import Foundation
+
+/**
+ LeanCloud access control lists type.
+
+ You can use it to set access control lists on an object.
+ */
+public final class LCACL: LCType {
+    typealias Access = [String: Bool]
+    typealias AccessTable = [String: Access]
+
+    var value: AccessTable = [:]
+
+    /// The key for public, aka, all users.
+    static let publicAccessKey = "*"
+
+    /// The key for `read` permission.
+    static let readPermissionKey = "read"
+
+    /// The key for `write` permission.
+    static let writePermissionKey = "write"
+
+    public override init() {
+        super.init()
+    }
+
+    init?(JSONValue: AnyObject) {
+        guard let value = JSONValue as? AccessTable else {
+            return nil
+        }
+
+        self.value = value
+    }
+
+    /**
+     Permission type.
+     */
+    public struct Permission: OptionSetType {
+        public let rawValue: UInt
+
+        public init(rawValue: UInt) {
+            self.rawValue = rawValue
+        }
+
+        public static let Read  = Permission(rawValue: 1 << 0)
+        public static let Write = Permission(rawValue: 1 << 1)
+    }
+
+    /**
+     Generate access key for role name.
+
+     - parameter roleName: The name of role.
+
+     - returns: An access key for role name.
+     */
+    static func accessKey(roleName roleName: String) -> String {
+        return "role:\(roleName)"
+    }
+
+    /**
+     Get access permission for public.
+
+     - parameter permission: The permission that you want to get.
+
+     - returns: true if the permission is allowed, false otherwise.
+     */
+    public func getAccess(permission: Permission) -> Bool {
+        return getAccess(permission, key: LCACL.publicAccessKey)
+    }
+
+    /**
+     Set access permission for public.
+
+     - parameter permission: The permission to be set.
+     - parameter allowed:    A boolean value indicates whether permission is allowed or not.
+     */
+    public func setAccess(permission: Permission, allowed: Bool) {
+        setAccess(permission, key: LCACL.publicAccessKey, allowed: allowed)
+    }
+
+    /**
+     Get access permission for user.
+
+     - parameter permission: The permission that you want to get.
+     - parameter userID:     The user object ID for which you want to get.
+
+     - returns: true if the permission is allowed, false otherwise.
+     */
+    public func getAccess(permission: Permission, forUserID userID: String) -> Bool {
+        return getAccess(permission, key: userID)
+    }
+
+    /**
+     Set access permission for user.
+
+     - parameter permission: The permission to be set.
+     - parameter allowed:    A boolean value indicates whether permission is allowed or not.
+     - parameter userID:     The user object ID for which the permission will be set.
+     */
+    public func setAccess(permission: Permission, allowed: Bool, forUserID userID: String) {
+        setAccess(permission, key: userID, allowed: allowed)
+    }
+
+    /**
+     Get access permission for role.
+
+     - parameter permission: The permission that you want to get.
+     - parameter roleName:   The role name for which you want to get.
+
+     - returns: true if the permission is allowed, false otherwise.
+     */
+    public func getAccess(permission: Permission, forRoleName roleName: String) -> Bool {
+        return getAccess(permission, key: LCACL.accessKey(roleName: roleName))
+    }
+
+    /**
+     Set access permission for role.
+
+     - parameter permission: The permission to be set.
+     - parameter allowed:    A boolean value indicates whether permission is allowed or not.
+     - parameter roleName:   The role name for which the permission will be set.
+     */
+    public func setAccess(permission: Permission, allowed: Bool, forRoleName roleName: String) {
+        setAccess(permission, key: LCACL.accessKey(roleName: roleName), allowed: allowed)
+    }
+
+    /**
+     Get access for key.
+
+     - parameter permission: The permission that you want to get.
+     - parameter key:        The key for which you want to get.
+
+     - returns: true if all permission is allowed, false otherwise.
+     */
+    func getAccess(permission: Permission, key: String) -> Bool {
+        guard let access = value[key] else {
+            return false
+        }
+
+        /* We use AND logic here. If any one of permissions is disallowed, return false. */
+        if permission.contains(.Read) {
+            if access[LCACL.readPermissionKey] == nil {
+                return false
+            }
+        }
+        if permission.contains(.Write) {
+            if access[LCACL.writePermissionKey] == nil {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    /**
+     Update permission for given key.
+
+     - parameter permission: The permission.
+     - parameter key:        The key for which the permission to be updated.
+     - parameter allowed:    A boolean value indicates whether permission is allowed or not.
+     */
+    func setAccess(permission: Permission, key: String, allowed: Bool) {
+        var access = value[key] ?? [:]
+
+        /* We reserve the allowed permissions only. */
+        if permission.contains(.Read) {
+            access[LCACL.readPermissionKey] = allowed ? allowed : nil
+        }
+        if permission.contains(.Write) {
+            access[LCACL.writePermissionKey] = allowed ? allowed : nil
+        }
+
+        value[key] = !access.isEmpty ? access : nil
+    }
+}
