20d71a4397972a109600f4a8aad16ff4892c61d0
Tianyong Tang <tang3w@gmail.com>
2016-07-08 17:28:45 +0800

    import Foundation instead of UIKit in umbrella header

--- Source/LeanCloud.h
+++ Source/LeanCloud.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 LeanCloud. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for LeanCloud.
 FOUNDATION_EXPORT double LeanCloudVersionNumber;
