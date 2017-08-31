dc22ad46c83e2ce58a0ced19233006be53ea9a01
Tang Tianyong <tang3w@gmail.com>
2017-08-31 11:23:26 +0800

    Support to log HTTP request and response

--- Sources/Storage/RESTClient.swift
+++ Sources/Storage/RESTClient.swift
@@ -209,12 +209,39 @@ class RESTClient {
         }
 
         let request = requestManager.request(urlString, method: method, parameters: parameters, encoding: encoding, headers: headers)
+        log(request)
 
         request.responseJSON(queue: dispatchQueue) { response in
+            log(request, response)
             completionHandler(LCResponse(response))
         }
 
         return LCRequest(request)
+    }
+
+    static func log(_ request: Request) {
+        var curl = request.debugDescription
+
+        if curl.hasPrefix("$ ") {
+            curl = curl.substring(from: curl.index(curl.startIndex, offsetBy: 2))
+        }
+
+        let sessionId = UInt(bitPattern: ObjectIdentifier(request))
+        let message = "------ BEGIN LeanCloud HTTP Request\n" +
+                      "session: \(sessionId)\n" +
+                      "curl: \(curl)\n" +
+                      "------ END"
+        print(message)
+    }
+
+    static func log(_ request: Request, _ response: DataResponse<Any>) {
+        let body = response.value ?? ""
+        let sessionId = UInt(bitPattern: ObjectIdentifier(request))
+        let message = "------ BEGIN LeanCloud HTTP Response\n" +
+                      "session: \(sessionId)\n" +
+                      "body: \(body)\n" +
+                      "------ END"
+        print(message)
     }
 
     /**
