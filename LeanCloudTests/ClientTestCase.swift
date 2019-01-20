298b7a4b1260ef1610580a281c71703a56b92027
zapcannon87 <zapcannon87@outlook.com>
2019-01-20 13:40:17 +0800

    Update test case

--- LeanCloudTests/ClientTestCase.swift
+++ LeanCloudTests/ClientTestCase.swift
@@ -16,48 +16,123 @@ class LCClientTestCase: BaseTestCase {
     }
     
     class Delegator: LCClientDelegate {
-        
         var clientEvent: ((_ client: LCClient, _ event: LCClientEvent) -> Void)?
-        
         func client(_ client: LCClient, event: LCClientEvent) {
             clientEvent?(client, event)
         }
-        
         var conversationEvent: ((_ client: LCClient, _ conversation: LCConversation, _ event: LCConversationEvent) -> Void)?
-        
         func client(_ client: LCClient, conversation: LCConversation, event: LCConversationEvent) {
             conversationEvent?(client, conversation, event)
         }
+    }
+    
+    func testClientInit() {
         
+        do {
+            let invalidID: String = Array<String>.init(repeating: "a", count: 65).joined()
+            let _ = try LCClient(ID: invalidID)
+            XCTFail()
+        } catch {
+            XCTAssertTrue(error is LCError)
+        }
+        
+        do {
+            let invalidTag: String = "default"
+            let _ = try LCClient(ID: "aaaaaa", tag: invalidTag)
+            XCTFail()
+        } catch {
+            XCTAssertTrue(error is LCError)
+        }
+        
+        do {
+            let _ = try LCClient(ID: "qweasd", tag: "mobile")
+        } catch {
+            XCTFail()
+        }
     }
 
     func testClientOpenAndClose() {
         
         let client: LCClient = try! LCClient(ID: uuid)
-        let count: Int = 3
         
-        for _ in 0..<count {
-            
-            let exp = expectation(description: "client open success")
+        for _ in 0..<3 {
+            let exp = expectation(description: "open and close")
             exp.expectedFulfillmentCount = 3
-            
             client.open { (result) in
                 XCTAssertTrue(Thread.isMainThread)
+                XCTAssertTrue(result.isSuccess)
                 XCTAssertNil(result.error)
+                XCTAssertNotNil(client.sessionToken)
+                XCTAssertNotNil(client.sessionTokenExpiration)
+                XCTAssertNil(client.openingOptions)
+                XCTAssertNil(client.openingCompletion)
+                XCTAssertEqual(client.sessionState, .opened)
                 exp.fulfill()
                 client.open { (result) in
                     XCTAssertNotNil(result.error)
                     exp.fulfill()
                     client.close() { (result) in
                         XCTAssertTrue(Thread.isMainThread)
+                        XCTAssertTrue(result.isSuccess)
                         XCTAssertNil(result.error)
+                        XCTAssertNil(client.sessionToken)
+                        XCTAssertNil(client.sessionTokenExpiration)
+                        XCTAssertNil(client.openingOptions)
+                        XCTAssertNil(client.openingCompletion)
+                        XCTAssertEqual(client.sessionState, .closed)
                         exp.fulfill()
                     }
                 }
             }
-            
             waitForExpectations(timeout: timeout, handler: nil)
         }
+    }
+    
+    func testClientDelegateEvent() {
+        
+        let client: LCClient = try! LCClient(ID: uuid)
+        let delegator: Delegator = Delegator()
+        client.delegate = delegator
+        
+        let openExp = expectation(description: "open")
+        client.open { (r) in
+            XCTAssertTrue(r.isSuccess)
+            openExp.fulfill()
+        }
+        wait(for: [openExp], timeout: timeout)
+        
+        let pauseExp = expectation(description: "pause")
+        delegator.clientEvent = { c, e in
+            XCTAssertTrue(Thread.isMainThread)
+            if c === client,
+                case .sessionDidPause(error: _) = e {
+                XCTAssertEqual(client.sessionState, .paused)
+                pauseExp.fulfill()
+            }
+        }
+        client.connection.disconnect()
+        wait(for: [pauseExp], timeout: timeout)
+        
+        let reopenExp = expectation(description: "resuming and reopen")
+        reopenExp.expectedFulfillmentCount = 2
+        reopenExp.assertForOverFulfill = true
+        delegator.clientEvent = { c, e in
+            XCTAssertTrue(Thread.isMainThread)
+            if c === client {
+                switch e {
+                case .sessionDidResume:
+                    XCTAssertEqual(client.sessionState, .resuming)
+                    reopenExp.fulfill()
+                case .sessionDidOpen:
+                    XCTAssertEqual(client.sessionState, .opened)
+                    reopenExp.fulfill()
+                default:
+                    XCTFail()
+                }
+            }
+        }
+        client.connection.connect()
+        wait(for: [reopenExp], timeout: timeout)
     }
     
     func testClientSessionConflict() {
@@ -82,12 +157,10 @@ class LCClientTestCase: BaseTestCase {
         )
         
         let exp1 = expectation(description: "client1 open success")
-        
         client1.open { (result) in
-            XCTAssertNil(result.error)
+            XCTAssertTrue(result.isSuccess)
             exp1.fulfill()
         }
-        
         wait(for: [exp1], timeout: timeout)
         
         let application2: LCApplication = LCApplication(
@@ -108,10 +181,8 @@ class LCClientTestCase: BaseTestCase {
         
         let exp2 = expectation(description: "client2 open success & kick client1 success")
         exp2.expectedFulfillmentCount = 2
-        
-        delegator1.clientEvent = { client, event in
-            XCTAssertTrue(Thread.isMainThread)
-            if client === client1,
+        delegator1.clientEvent = { c, event in
+            if c === client1,
                 case let .sessionDidClose(error: error) = event {
                 XCTAssertEqual(
                     error.code,
@@ -121,15 +192,13 @@ class LCClientTestCase: BaseTestCase {
             }
         }
         client2.open { (result) in
-            XCTAssertNil(result.error)
+            XCTAssertTrue(result.isSuccess)
             exp2.fulfill()
         }
-        
         wait(for: [exp2], timeout: timeout)
         
         let exp3 = expectation(description: "client1 resume with deviceToken1 fail, and set deviceToken2 then resume success")
         exp3.expectedFulfillmentCount = 2
-        
         client1.open(options: []) { (result) in
             XCTAssertEqual(
                 result.error?.code,
@@ -145,8 +214,56 @@ class LCClientTestCase: BaseTestCase {
             }
             exp3.fulfill()
         }
-        
         wait(for: [exp3], timeout: timeout)
+    }
+    
+    func testClientSessionTokenExpired() {
+        
+        let client: LCClient = try! LCClient(ID: uuid)
+        let delegator: Delegator = Delegator()
+        client.delegate = delegator
+        
+        let openExp = expectation(description: "open")
+        client.open { (result) in
+            XCTAssertTrue(result.isSuccess)
+            openExp.fulfill()
+        }
+        wait(for: [openExp], timeout: timeout)
+        
+        client.sessionToken = uuid
+        
+        let exp = expectation(description: "Pause, Resume, First Reopen Then Session Token Expired and Second Reopen Success")
+        exp.expectedFulfillmentCount = 4
+        exp.assertForOverFulfill = true
+        delegator.clientEvent = { c, event in
+            if c === client {
+                switch event {
+                case .sessionDidPause(error: _):
+                    exp.fulfill()
+                case .sessionDidResume:
+                    exp.fulfill()
+                case .sessionDidOpen:
+                    exp.fulfill()
+                default:
+                    XCTFail()
+                }
+            }
+        }
+        let _ = NotificationCenter.default.addObserver(
+            forName: LCClient.TestSessionTokenExpiredNotification,
+            object: client,
+            queue: OperationQueue.main
+        ) { (notification) in
+            let error = notification.userInfo?["error"] as? LCError
+            XCTAssertEqual(
+                error?.code,
+                LCError.ServerErrorCode.sessionTokenExpired.rawValue
+            )
+            exp.fulfill()
+        }
+        client.connection.disconnect()
+        client.connection.connect()
+        wait(for: [exp], timeout: timeout)
     }
     
     func testClientReportDeviceToken() {
@@ -155,19 +272,17 @@ class LCClientTestCase: BaseTestCase {
         
         let exp = expectation(description: "client report device token success")
         exp.expectedFulfillmentCount = 2
-        
         client.open { (result) in
-            XCTAssertNil(result.error)
+            XCTAssertTrue(result.isSuccess)
             client.installation.set(deviceToken: self.uuid, apnsTeamId: "")
             exp.fulfill()
         }
         let _ = NotificationCenter.default.addObserver(forName: LCClient.TestReportDeviceTokenNotification, object: client, queue: OperationQueue.main) { (notification) in
-            let result = notification.userInfo?["result"] as! Connection.CommandCallback.Result
-            XCTAssertEqual(result.command?.cmd, .report)
-            XCTAssertEqual(result.command?.op, .uploaded)
+            let result = notification.userInfo?["result"] as? Connection.CommandCallback.Result
+            XCTAssertEqual(result?.command?.cmd, .report)
+            XCTAssertEqual(result?.command?.op, .uploaded)
             exp.fulfill()
         }
-        
         waitForExpectations(timeout: timeout, handler: nil)
     }
 
