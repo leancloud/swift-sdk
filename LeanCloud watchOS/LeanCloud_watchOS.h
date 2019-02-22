1bd0cc0e1101f71915bdfdab634fe879e3c1227a
zapcannon87 <zapcannon87@outlook.com>
2019-02-22 14:12:17 +0800

    Add targets: macOS, tvOS, watchOS

---
+++ LeanCloud watchOS/LeanCloud_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  LeanCloud_watchOS.h
+//  LeanCloud watchOS
+//
+//  Created by zapcannon87 on 2019/2/22.
+//  Copyright © 2019 LeanCloud. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for LeanCloud_watchOS.
+FOUNDATION_EXPORT double LeanCloud_watchOSVersionNumber;
+
+//! Project version string for LeanCloud_watchOS.
+FOUNDATION_EXPORT const unsigned char LeanCloud_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LeanCloud_watchOS/PublicHeader.h>
+
+
