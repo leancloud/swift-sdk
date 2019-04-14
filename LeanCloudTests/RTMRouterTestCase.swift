a0130ec353ce6057c8d56e427f36646868735951
zapcannon87 <zapcannon87@outlook.com>
2019-04-14 14:51:26 +0800

    test: Update for RTMRouter

--- LeanCloudTests/RTMRouterTestCase.swift
+++ LeanCloudTests/RTMRouterTestCase.swift
@@ -10,32 +10,50 @@ import XCTest
 @testable import LeanCloud
 
 class RTMRouterTestCase: RTMBaseTestCase {
-
-    func testRouter() {
-        let application = LCApplication.default
-
-        let router = RTMRouter(application: application)
-        let routerCache = router.cache
-
-        try! routerCache.clear()
-        XCTAssertNil(try! routerCache.getRoutingTable())
-
-        var result: LCGenericResult<RTMRoutingTable>!
-
-        router.route { aResult in
-            result = aResult
+    
+    func test() {
+        
+        let router = try! RTMRouter(application: .default)
+        let fileURL = router.tableCacheURL!
+        
+        XCTAssertTrue(fileURL.path.contains("Library/Caches"))
+        XCTAssertTrue(fileURL.path.contains(LocalStorageContext.Module.router.path))
+        XCTAssertTrue(fileURL.path.contains(LocalStorageContext.File.rtmServer.name))
+        
+        router.clearTableCache()
+        
+        XCTAssertNil(router.table)
+        XCTAssertFalse(FileManager.default.fileExists(atPath: fileURL.path))
+        
+        let date = Date()
+        
+        let exp = expectation(description: "routing")
+        router.route { (direct, result) in
+            XCTAssertTrue(result.isSuccess)
+            XCTAssertNil(result.error)
+            XCTAssertFalse(direct)
+            exp.fulfill()
         }
-
-        busywait { result != nil }
-
-        switch result! {
-        case .success(let routingTable):
-            XCTAssertNotNil(routingTable.primary)
-        case .failure(let error):
-            XCTFail(error.localizedDescription)
+        wait(for: [exp], timeout: timeout)
+        
+        XCTAssertNotNil(router.table)
+        XCTAssertNotNil(router.table?.secondary)
+        XCTAssertEqual(router.table?.primary, router.table?.primaryURL?.absoluteString)
+        XCTAssertEqual(router.table?.secondary, router.table?.secondaryURL?.absoluteString)
+        XCTAssertTrue((router.table?.ttl ?? 0) > 0)
+        XCTAssertEqual(router.table?.continuousFailureCount, 0)
+        XCTAssertTrue(FileManager.default.fileExists(atPath: fileURL.path))
+        XCTAssertGreaterThan(router.table?.createdTimestamp ?? 0, date.timeIntervalSince1970)
+        XCTAssertEqual(router.table?.isExpired, false)
+        
+        router.updateFailureCount()
+        XCTAssertEqual(router.table?.continuousFailureCount, 1)
+        router.updateFailureCount(reset: true)
+        XCTAssertEqual(router.table?.continuousFailureCount, 0)
+        for _ in 0..<10 {
+            router.updateFailureCount()
         }
-
-        XCTAssertNotNil(try! routerCache.getRoutingTable())
+        XCTAssertEqual(router.table?.shouldClear, true)
     }
 
 }
