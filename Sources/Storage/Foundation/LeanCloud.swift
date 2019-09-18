cf48cf71caa105ca8ba243636d33684e1b6d0304
zapcannon87 <zapcannon87@outlook.com>
2019-09-18 12:32:29 +0800

    release: 16.5.1

--- Sources/Storage/Foundation/LeanCloud.swift
+++ Sources/Storage/Foundation/LeanCloud.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public let version = "16.5.0"
+public let version = "16.5.1"
 
 /// `version` is a common word, so use `__LeanCloudVersion` to wrap it to avoid conflict.
 var __LeanCloudVersion: String {
