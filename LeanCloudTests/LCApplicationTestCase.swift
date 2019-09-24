88a33f2d709ac70eccc21545b235112eabc887b4
zapcannon87 <zapcannon87@outlook.com>
2019-09-24 19:27:49 +0800

    test: update application case

--- LeanCloudTests/LCApplicationTestCase.swift
+++ LeanCloudTests/LCApplicationTestCase.swift
@@ -10,5 +10,150 @@ import XCTest
 @testable import LeanCloud
 
 class LCApplicationTestCase: BaseTestCase {
+    
+    func testRegistry() {
+        XCTAssertTrue(LCApplication.registry[LCApplication.default.id] === LCApplication.default)
+    }
+    
+    func testLogLevel() {
+        Array<(LCApplication.LogLevel, LCApplication.LogLevel)>([
+            (.all, .verbose),
+            (.verbose, .debug),
+            (.debug, .error),
+            (.error, .off)
+        ]).forEach { (left, right) in
+            XCTAssertGreaterThan(left, right)
+        }
+    }
+    
+    func testBasic() {
+        if [.cn, .ce].contains(LCApplication.default.region) {
+            XCTAssertNotNil(LCApplication.default.id)
+            XCTAssertNotNil(LCApplication.default.key)
+            XCTAssertNotNil(LCApplication.default.serverURL)
+        } else if [.us].contains(LCApplication.default.region) {
+            XCTAssertNotNil(LCApplication.default.id)
+            XCTAssertNotNil(LCApplication.default.key)
+        }
+    }
+    
+    func testServerCustomizableModule() {
+        let host = "avoscloud.com"
+        let config = LCApplication.Configuration(
+            customizedServers: [
+                .api(host),
+                .push(host),
+                .rtm(host),
+                .engine(host)])
+        let app = try! LCApplication(
+            id: UUID().uuidString,
+            key: UUID().uuidString,
+            serverURL: "leancloud.cn",
+            configuration: config)
+
+        Array<AppRouter.Module>([.api, .push, .rtm, .engine]).forEach { (module) in
+            if module == .rtm {
+                XCTAssertEqual(
+                    app.appRouter.route(path: "foo", module: module),
+                    URL(string: "https://\(host)/foo"))
+            } else {
+                XCTAssertEqual(
+                    app.appRouter.route(path: "foo", module: module),
+                    URL(string: "https://\(host)/\(AppRouter.Configuration.default.apiVersion)/foo"))
+            }
+        }
+        
+        LCApplication.registry.removeValue(forKey: app.id)
+    }
+    
+    func testEnvironment() {
+        let config = LCApplication.Configuration(
+            environment: [
+                .cloudEngineDevelopment,
+                .pushDevelopment])
+        let app = try! LCApplication(
+            id: UUID().uuidString,
+            key: UUID().uuidString,
+            serverURL: "leancloud.cn",
+            configuration: config)
+        
+        XCTAssertEqual(app.cloudEngineMode, "0")
+        XCTAssertEqual(app.pushMode, "dev")
+        
+        try! app.set(
+            id: app.id,
+            key: app.key,
+            serverURL: app.serverURL,
+            configuration: LCApplication.Configuration(
+                environment: .default))
+        
+        XCTAssertEqual(app.cloudEngineMode, "1")
+        XCTAssertEqual(app.pushMode, "prod")
+        
+        LCApplication.registry.removeValue(forKey: app.id)
+    }
+    
+    func testRegion() {
+        Array<(String, LCApplication.Region)>([
+            (UUID().uuidString + "-gzGzoHsz", .cn),
+            (UUID().uuidString.replacingOccurrences(of: "-", with: ""), .cn),
+            (UUID().uuidString + "-9Nh9j0Va", .ce),
+            (UUID().uuidString + "-MdYXbMMI", .us)
+        ]).forEach { (id, region) in
+            let app = try! LCApplication(
+                id: id,
+                key: UUID().uuidString,
+                serverURL: "leancloud.cn")
+            XCTAssertEqual(app.region, region)
+            LCApplication.registry.removeValue(forKey: app.id)
+        }
+    }
+    
+    func testInit() {
+        Array([
+            UUID().uuidString + "-gzGzoHsz",
+            UUID().uuidString.replacingOccurrences(of: "-", with: ""),
+            UUID().uuidString + "-9Nh9j0Va"
+        ]).forEach { (id) in
+            do {
+                _ = try LCApplication(
+                    id: id,
+                    key: UUID().uuidString,
+                    serverURL: nil)
+                XCTFail()
+            } catch {
+                XCTAssertTrue(error is LCError)
+            }
+            do {
+                let app = try LCApplication(
+                    id: id,
+                    key: UUID().uuidString,
+                    serverURL: "leancloud.cn")
+                Array<AppRouter.Module>([.api, .push, .rtm, .engine]).forEach { (module) in
+                    if module == .rtm {
+                        XCTAssertEqual(
+                            app.appRouter.route(path: "foo", module: module),
+                            URL(string: "https://leancloud.cn/foo"))
+                    } else {
+                        XCTAssertEqual(
+                            app.appRouter.route(path: "foo", module: module),
+                            URL(string: "https://leancloud.cn/\(AppRouter.Configuration.default.apiVersion)/foo"))
+                    }
+                }
+                LCApplication.registry.removeValue(forKey: app.id)
+            } catch {
+                XCTFail("\(error)")
+            }
+        }
+        
+        do {
+            let app = try LCApplication(
+                id: UUID().uuidString + "-MdYXbMMI",
+                key: UUID().uuidString)
+            LCApplication.registry.removeValue(forKey: app.id)
+        } catch {
+            XCTFail("\(error)")
+        }
+    }
 
 }
