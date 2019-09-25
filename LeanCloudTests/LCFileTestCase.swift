a4722996acb37b1973033ad309f503d48b5d902d
zapcannon87 <zapcannon87@outlook.com>
2019-09-25 20:58:02 +0800

    test: add case for file

--- LeanCloudTests/LCFileTestCase.swift
+++ LeanCloudTests/LCFileTestCase.swift
@@ -11,14 +11,9 @@ import XCTest
 
 class LCFileTestCase: BaseTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testSave() {
+        let fileURL = bundleResourceURL(name: "test", ext: "png")
+        XCTAssertTrue(LCFile(payload: .fileURL(fileURL: fileURL)).save().isSuccess)
     }
     
     func upload(payload: LCFile.Payload) {
