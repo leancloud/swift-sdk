02cc3adbe8c6fd47754195599263afd0b5d1d02a
zapcannon87 <zapcannon87@outlook.com>
2020-02-05 18:13:49 +0800

    fix(date): format representation string of date is incorrect in some region
    close #321

--- Sources/Foundation/Date.swift
+++ Sources/Foundation/Date.swift
@@ -18,7 +18,8 @@ public final class LCDate: NSObject, LCValue, LCValueExtension {
 
     static let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.SSS'Z'"
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
         return formatter
     }()
